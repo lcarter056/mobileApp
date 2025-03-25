2ee6f189256a8f858bcf7de40aef509efc00a298
lcarter056 <94881226+lcarter056@users.noreply.github.com>
2025-03-25 15:28:13 -0500

    starter app

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => {
+    alert('Button Pressed!');
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click Here" onPress ={handlePress} />
       <StatusBar style="auto" />
     </View>
   );
